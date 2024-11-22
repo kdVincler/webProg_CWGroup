236c2d5516cd6a95d275eeff65881521a68c7412
ec22761 <ec22761@qmul.ac.uk>
2024-11-22 13:28:53 +0000

    Added basic temp navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,37 @@
 <template>
-    <main class="container pt-4">
-        <div>
-            <router-link
-                class=""
-                :to="{name: 'Main Page'}"
-            >
-                Main Page
-            </router-link>
-            |
-            <router-link
-                class=""
-                :to="{name: 'Other Page'}"
-            >
-                Other Page
-            </router-link>
+  <div class="navbar bg-base-200">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Hobby App</a>
+    </div>
+    <div class="flex-none gap-2">
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full bg-red-400 ">
+              <!--TODO: Add User Initials or profile picture-->
+            <div class="flex flex-row items-center justify-center h-full w-full">
+              <span class="w-full text-center">RP</span>
+            </div>
+
+          </div>
         </div>
-        <RouterView class="flex-shrink-0" />
-    </main>
+        <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/other">Other</RouterLink></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <RouterView/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { RouterView } from "vue-router";
+import {defineComponent} from "vue";
+import {RouterView} from "vue-router";
 
 export default defineComponent({
-    components: { RouterView },
+  components: {RouterView},
 });
 
 </script>
