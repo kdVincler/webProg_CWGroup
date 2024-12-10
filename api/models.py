0da6c9aad152a22eda8f8d587591c68a772578f1
ec22741 <ec22741@qmul.ac.uk>
2024-12-10 20:32:47 +0000

    User model skeleton

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
 
@@ -8,3 +10,6 @@ class PageView(models.Model):
 
     def __str__(self):
         return f"Page view count: {self.count}"
+    
+class User(AbstractUser):
+    pass
