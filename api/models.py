f94b0a1068ceaec767b17824dd9830bc50973c97
ec22741 <ec22741@qmul.ac.uk>
2024-12-10 22:25:23 +0000

    created user model and view functions for requests

--- api/models.py
+++ api/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-
 class PageView(models.Model):
     count = models.IntegerField(default=0)
 
@@ -12,4 +11,22 @@ class PageView(models.Model):
         return f"Page view count: {self.count}"
     
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    # hobbies as another class ?
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        """String representation of the user"""
+        return self.email
+    
+    def as_dict(self):
+        """JSON representation of the user"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "date_of_birth": self.date_of_birth,
+            "password": self.password
+        }
--- api/views.py
+++ api/views.py
@@ -1,6 +1,68 @@
+import json
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render
+from .models import User
 
 
 def main_spa(request: HttpRequest) -> HttpResponse:
     return render(request, 'api/spa/index.html', {})
+
+# USER MODEL VIEWS
+
+def user_list_view(request):
+    """API endpoint for collection of users"""
+    if request.method == 'GET':
+        # getting all users
+        return JsonResponse({
+            'users': 
+                [user.as_dict() for user in User.objects.all()]
+        })
+    elif request.method == 'POST':
+        # adding a user
+        return add_user(request)
+    else:
+        return HttpResponse(status=405)
+    
+def user_api(request, user_id):
+    """API endpoint for a single user"""
+    user = User.objects.get(id=user_id)
+    if request.method == 'PUT':
+        # updating a user
+        return update_user(request, user_id)
+    if request.method == 'DELETE':
+        return delete_user(request, user_id)
+    return JsonResponse(user.as_dict())
+
+def add_user(request):
+    """Add a user to the database"""
+    try:
+        data = json.loads(request.body)
+        user = User.objects.create(
+            name = data['name'],
+            email = data['email'],
+            date_of_birth = data['date_of_birth'],
+            password = data['password']
+        )
+        return JsonResponse(user.as_dict())
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+    
+def update_user(request, user_id):
+    """Update a user's details in the database"""
+    user = User.objects.get(id=user_id)
+    try:
+        data = json.loads(request.body)
+        user.name = data.get('name', user.name)
+        user.email = data.get('email', user.email)
+        user.date_of_birth = data.get('date_of_birth', user.date_of_birth)
+        user.password = data.get('password', user.password)
+        user.save()
+        return JsonResponse(user.as_dict())
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+    
+def delete_user(request, user_id):
+    """Delete a user from the database"""
+    user = User.objects.get(id=user_id)
+    user.delete()
+    return JsonResponse({'message': 'User deleted successfully!'})
