17e108b30934a4ea6fd97c07cab705f58ee5f142
ec22741 <ec22741@qmul.ac.uk>
2024-12-12 15:06:51 +0000

    added Hobby model and through model to connect User and Hobby

--- api/models.py
+++ api/models.py
@@ -9,13 +9,33 @@ class PageView(models.Model):
 
     def __str__(self):
         return f"Page view count: {self.count}"
+
+class Hobby(models.Model):
+    """Hobby model"""
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        """String representation of the hobby"""
+        return self.name
     
+    def as_dict(self):
+        """JSON representation of the hobby"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+        }
+     
 class User(AbstractUser):
+    """User model"""
     name = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(blank=True, null=True)
-    # hobbies as another class ?
     password = models.CharField(max_length=255)
+
+    # Many-to-Many relationship with Hobby
+    hobbies = models.ManyToManyField(Hobby, through='UserHobby')
 
     def __str__(self):
         """String representation of the user"""
@@ -30,3 +50,20 @@ class User(AbstractUser):
             "date_of_birth": self.date_of_birth,
             "password": self.password
         }
+
+class UserHobby(models.Model):
+    """Model representing the relationship between a user and a hobby"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hobby = models.ForeignKey(Hobby, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """String representation of the user-hobby relationship"""
+        return f"{self.user} - {self.hobby}"
+    
+    def as_dict(self):
+        """JSON representation of the user-hobby relationship"""
+        return {
+            "user": self.user.as_dict(),
+            "hobby": self.hobby.as_dict(),
+        }
+  
