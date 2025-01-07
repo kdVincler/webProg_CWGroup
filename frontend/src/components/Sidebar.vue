d4d718190827571562905aa45f6425b0cb53dfab
ec22761 <ec22761@qmul.ac.uk>
2025-01-07 11:11:53 +0000

    Added logout button to sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,14 +1,18 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
+import {logout} from "../api";
 
 export default defineComponent({
-  name: "Sidebar"
+  name: "Sidebar",
+  methods: {
+    logout,
+  }
 })
 </script>
 
 <template>
-  <div class="h-screen bg-slate-600 w-1/6 px-6 py-2 text-base-200">
-    <RouterLink to="/" class="btn btn-ghost text-xl italic -mx-4">Hobbies 24</RouterLink>
+  <div class="h-screen bg-slate-600 w-1/6 px-6 py-2 text-base-200 flex flex-col items-start">
+    <RouterLink to="/" class="btn btn-ghost text-xl italic">Hobbies 24</RouterLink>
     <div class="collapse collapse-arrow bg-slate-600 my-2">
       <input type="radio" name="my-accordion-2"/>
       <div class="collapse-title text-md font-medium">My Hobbies</div>
@@ -30,6 +34,7 @@ export default defineComponent({
         <p>List of friend requests goes here!</p>
       </div>
     </div>
+    <button @click="logout" class="btn w-full mt-auto mb-4 bg-slate-500 border-0 text-white hover:bg-slate-400">Logout</button>
   </div>
 
 </template>
