2718d23e0b20b9c334b1ec3cfbb417970d794a61
ec22761 <ec22761@qmul.ac.uk>
2025-01-08 09:26:27 +0000

    Started on modal, push for Alicia

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -14,7 +14,10 @@ export default defineComponent({
   methods: {
     logout,
     addUserHobby,
-    deleteUserHobby
+    deleteUserHobby,
+    showModal() {
+      document.getElementById('add_hobby')?.showModal();
+    }
   }
 })
 </script>
@@ -28,14 +31,30 @@ export default defineComponent({
       <div class="collapse-content">
         <ul class="flex flex-col gap-1 mb-4 ">
           <button @click="deleteUserHobby(hobby.id)" v-for="hobby in userStore.getHobbies" :key="hobby.id"
-              class="capitalize text-md font-medium btn bg-slate-700 border-0 hover:bg-slate-500 text-white justify-between">
-            {{ hobby.name }}{{hobby.id}}<Trash size="16"/>
+                  class="capitalize text-md font-medium btn bg-slate-700 border-0 hover:bg-slate-500 text-white justify-between">
+            {{ hobby.name }}{{ hobby.id }}
+            <Trash size="16"/>
           </button>
         </ul>
-        <button @click="addUserHobby(Math.random().toString(36).substring(7))"
+        <button @click="showModal"
                 class="btn w-full mt-auto mb-4 bg-slate-500 border-0 text-white hover:bg-slate-400">Add Hobby
         </button>
-
+        <dialog id="add_hobby" className="modal text-neutral-700">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+            <div class="flex w-full mt-6">
+              <div class="card rounded-box flex-grow place-items-center min-h-20 w-1/2">
+                <h1>Select an Existing Hobby</h1>
+              </div>
+              <div class="divider divider-horizontal">OR</div>
+              <div class="card rounded-box flex-grow place-items-center min-h-20 w-1/2">
+                <h1>Add a New Hobby</h1>
+              </div>
+            </div>
+          </div>
+        </dialog>
       </div>
     </div>
     <div class="collapse collapse-arrow bg-slate-600 my-2">
