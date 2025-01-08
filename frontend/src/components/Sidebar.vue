e56ddd689eba345a9c4b83b67cec653b9ec63e73
ec22761 <ec22761@qmul.ac.uk>
2025-01-08 09:13:30 +0000

    Visual bug fix

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -23,9 +23,9 @@ export default defineComponent({
   <div class="h-screen bg-slate-600 w-1/6 px-6 py-2 text-base-200 flex flex-col items-start">
     <RouterLink to="/" class="btn btn-ghost text-xl italic">Hobby 24</RouterLink>
     <div class="collapse collapse-arrow bg-slate-600 my-2">
-      <input type="radio" name="my-accordion-2 "/>
+      <input type="radio" name="my-accordion-2"/>
       <div class="collapse-title text-md font-medium">My Hobbies</div>
-      <div class="collapse-content ">
+      <div class="collapse-content">
         <ul class="flex flex-col gap-1 mb-4 ">
           <button @click="deleteUserHobby(hobby.id)" v-for="hobby in userStore.getHobbies" :key="hobby.id"
               class="capitalize text-md font-medium btn bg-slate-700 border-0 hover:bg-slate-500 text-white justify-between">
