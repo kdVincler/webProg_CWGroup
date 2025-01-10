e510ab434ced5f83cb2a5f89211444bad7d12742
ec22741 <ec22741@qmul.ac.uk>
2025-01-10 22:49:59 +0000

    user serialiser implementation

--- api/serializers.py
+++ api/serializers.py
@@ -17,8 +17,26 @@ class HobbySerializer(serializers.ModelSerializer):
         return instance
     
 class UserSerializer(serializers.ModelSerializer):
-    TODO
-    pass
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255, required=False)
+    email = serializers.EmailField(unique=True)
+    date_of_birth = serializers.DateField(required=False)
+
+    hobbies = HobbySerializer(many=True, required=False)
+    friends = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        return User.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """Update and return an existing user"""
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.date_of_birth = validated_data.get('date_of_birth', instance.date_of_birth)
+        instance.save()
+        return instance
+
 
 class UserHobbySerializer(serializers.ModelSerializer):
     TODO
